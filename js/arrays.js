b2c888d4e6ca6b5dd5af16854c6fed3455f6e5ac
uLemos <fekelton2012@hotmail.com>
2023-09-09 23:15:57 -0300

    :bug: Correção de commit

---
+++ js/arrays.js
@@ -0,0 +1,8 @@
+
+let friends = ['eu', ' você', ' nós', ' todos'];
+console.log(friends);
+
+friends[2] = 'Trevisano';
+console.log(friends[2]);
+
+document.getElementById('resultado').innerHTML = friends;
