8c6c53c4e37fbae37416a4e5a6492680c1aec4a8
uLemos <fekelton2012@hotmail.com>
2023-09-10 15:05:23 -0300

    :memo: Guess the number

---
+++ js/adivinhe.js
@@ -0,0 +1,18 @@
+let randomNumber = Math.floor(Math.random() * 100);
+let count = 0;
+
+function checkGuess(){
+    let number = document.getElementById('guess').value
+    count += 1;
+
+    if(number < randomNumber){
+        return document.getElementById('message').innerHTML = 'Too low, try again!'
+    }
+    else if(number > randomNumber){
+        return document.getElementById('message').innerHTML = 'Too high, try again'
+    }
+    else if(number == randomNumber){
+        return document.getElementById('message').innerHTML = `Congratulations, the number is ${number}, ${count} tentativas!`; 
+    }
+}
+    
