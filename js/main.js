2bcd9a7fc20eb2acd606277b421178349dc955fa
uLemos <fekelton2012@hotmail.com>
2023-09-11 02:41:15 -0300

    :memo: change img

--- js/main.js
+++ js/main.js
@@ -63,3 +63,21 @@
 
 // document.getElementById('listaCores').innerHTML = colorList;
 
+const images = [
+    "../images/SKYLINE-1-1.webp",
+    "../images/porsche.jpg",
+    "../images/coffee.avif"
+]
+let currentIndex = 0;
+
+let imageElement = document.getElementById('image');
+let buttonElement = document.getElementById('button');
+
+buttonElement.addEventListener('click', function () {
+    currentIndex += 1;
+    if(currentIndex >= images.length){
+        currentIndex = 0;
+    }
+    
+    imageElement.src = images[currentIndex];
+})
