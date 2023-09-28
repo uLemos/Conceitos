f3a89ff334124ddc2dea4453324fda2323b5ee4e
uLemos <fekelton2012@hotmail.com>
2023-09-28 09:22:32 -0300

    :memo: StopWatch JS

---
+++ js/stopWatch.js
@@ -0,0 +1,7 @@
+const display = document.getElementById('display');
+const startButton = document.getElementById('start');
+const stopButton = document.getElementById('stop');
+const resetButton = document.getElementById('reset');
+
+let intervalId;
+let elapsedTime = 0;
