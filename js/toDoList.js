c0270eab9382c8197229ca8382b0c6f60c721e00
uLemos <fsnvag12@gmail.com>
2023-10-02 18:53:43 -0300

    :memo: ToDo List

--- js/toDoList.js
+++ js/toDoList.js
@@ -1,22 +1,23 @@
-document.querySelector('#push').onclick = () => {
-    if(document.querySelector('#newTask input').value.length == 0){
-        alert('Kindly Enter Task Name!!!!');
+document.querySelector('#push').onclick = function(){
+    if(document.querySelector('#newtask input').value.length == 0){
+        alert("Kindly Enter Task Name!!!!")
     }
+
     else{
-        document.querySelector('#tasks').innerHTML += `<div class="newTask">
-            <span id="taskname">
-                ${document.querySelector('#newtask input').value}
-            </span>
-            <button class="delete">
-                <i class="far fa-trash-alt"></i>
-            </button>
-        </div>    
+        document.querySelector('#tasks').innerHTML += `
+            <div class="task">
+                <span id="taskname">
+                    ${document.querySelector('#newtask input').value}
+                </span>
+                <button class="delete">
+                    <i class="far fa-trash-alt"></i>
+                </button>
+            </div>
         `;
 
-        var current_tasks = document.querySelectorAll('.delete');
-
-        for(let i = 0; i < current_tasks.length; i++){
-            current_tasks[i].onclick = () => {
+        var current_tasks = document.querySelectorAll(".delete");
+        for(var i=0; i<current_tasks.length; i++){
+            current_tasks[i].onclick = function(){
                 this.parentNode.remove();
             }
         }
